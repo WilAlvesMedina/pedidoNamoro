1d0da252da0b5246b59101bc630afe5d81ebe0ff
Wilton <wiltonalvesmedina@gmail.com>
2022-11-30 11:38:36 -0300

    Inicio

---
+++ js/main.js
@@ -0,0 +1,40 @@
+const no01 = document.querySelector(".no1");
+const no02 = document.querySelector(".no2");
+const no03 = document.querySelector(".no3");
+const no04 = document.querySelector(".no4");
+const no05 = document.querySelector(".no5");
+
+no03.addEventListener("click", trocaNo);
+
+function trocaNo(){
+    no03.classList.remove("show");
+    no01.classList.add("show");
+}
+
+no01.addEventListener("click", trocaNo2);
+
+function trocaNo2(){
+    no01.classList.remove("show");
+    no04.classList.add("show");
+}
+
+no04.addEventListener("click", trocaNo3);
+
+function trocaNo3(){
+    no04.classList.remove("show");
+    no02.classList.add("show");
+}
+
+no02.addEventListener("click", trocaNo4);
+
+function trocaNo4(){
+    no02.classList.remove("show");
+    no05.classList.add("show");
+}
+
+no05.addEventListener("click", trocaNo5);
+
+function trocaNo5(){
+    no05.classList.remove("show");
+    no03.classList.add("show");
+}
