8a2f3828eaa7a3ad6d21ac5b05c3ef8aa02ad332
Wilton <wiltonalvesmedina@gmail.com>
2022-11-30 12:41:07 -0300

    Ajustes
    ajustes no js e css

--- js/main.js
+++ js/main.js
@@ -4,35 +4,35 @@ const no03 = document.querySelector(".no3");
 const no04 = document.querySelector(".no4");
 const no05 = document.querySelector(".no5");
 
-no03.addEventListener("click", trocaNo);
+no03.addEventListener("mouseover", trocaNo);
 
 function trocaNo(){
     no03.classList.remove("show");
     no01.classList.add("show");
 }
 
-no01.addEventListener("click", trocaNo2);
+no01.addEventListener("mouseover", trocaNo2);
 
 function trocaNo2(){
     no01.classList.remove("show");
     no04.classList.add("show");
 }
 
-no04.addEventListener("click", trocaNo3);
+no04.addEventListener("mouseover", trocaNo3);
 
 function trocaNo3(){
     no04.classList.remove("show");
     no02.classList.add("show");
 }
 
-no02.addEventListener("click", trocaNo4);
+no02.addEventListener("mouseover", trocaNo4);
 
 function trocaNo4(){
     no02.classList.remove("show");
     no05.classList.add("show");
 }
 
-no05.addEventListener("click", trocaNo5);
+no05.addEventListener("mouseover", trocaNo5);
 
 function trocaNo5(){
     no05.classList.remove("show");
